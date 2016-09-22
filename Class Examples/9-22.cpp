b89ff5375a091fe9ddbdd46efa067b74070fced5
travesto <tdc12c@acu.edu>
2016-09-22 12:04:41 -0500

    more examples

--- Class Examples/9-22.cpp
+++ Class Examples/9-22.cpp
@@ -11,33 +11,99 @@ using namespace std;
 // {
 
 // }
-class Logger
-{
-    private:
+////////////////////////////////////
+// class Logger
+// {
+//     private:
         
-};
-class Builder
-{
+// };
+// class Builder
+// {
 
+// };
+// class Pluginloader
+// {
+//     public:
+//         Logger* const p_Logger;
+//         Builder* const p_Builder;
+//         Pluginloader(Builder* const pBuilder);
+// };
+
+// Pluginloader::Pluginloader(Builder* const pBuilder) : p_Builder(pBuilder), p_Logger(p_Builder->GetLogger())
+// {
+//     /*
+//     Initialization happens in the order in the class (p_logger initialized before p_Builder).
+//     To fix this change the order in the class. Or just use pBuilder  instead of p_builder.
+//     */
+// }
+//////////////////////
+/*
+class A {
+    private:
+        int a;
+        const int q;
+    public:
+        A(int _a) : q(5)
+        {
+            a= _a; 
+            cout << "base initialized with " << _a << endl;
+        }
 };
-class Pluginloader
+class B : public A {
+    public:
+        B(int b) : A(b) {cout << "derived initizialed with " << b << endl;}
+};
+*/
+class badGuy
 {
     public:
-        Logger* const p_Logger;
-        Builder* const p_Builder;
-        Pluginloader(Builder* const pBuilder);
+        virtual void doAttack() = 0; //{ cout << "Base class attack!" << endl;}
 };
-
-Pluginloader::Pluginloader(Builder* const pBuilder) : p_Builder(pBuilder), p_Logger(p_Builder->GetLogger())
+class Ninja : public badGuy
 {
-    /*
-    Initialization happens in the order in the class (p_logger initialized before p_Builder).
-    To fix this change the order in the class. Or just use pBuilder  instead of p_builder.
-    */
-}
-
+    public:
+        void doAttack() {cout << "Ninja attack!" << endl;}
+};
+class Pirate : public badGuy
+{
+    public:
+        void doAttack() {cout << "Pirate attack!" << endl;}
+};
+class Outlaw : public badGuy
+{
+    public:
+        void doAttack() {cout << "Outlaw attack!" << endl;}
+};
 int main()
 {
-    A* a = new D();
-    a->eat();
+    int count = 0;
+    badGuy* list[10];
+    list[count++] = new Ninja();
+    list[count++] = new Ninja();
+    list[count++] = new Pirate();
+    list[count++] = new Ninja();
+    list[count++] = new Outlaw();
+    list[count++] = new Pirate();
+    list[count++] = new Pirate();
+    list[count++] = new Ninja();
+    list[count++] = new Outlaw();
+    list[count++] = new Ninja();
+
+    for (int i = 0; i <10; i++)
+    {
+        list[i]->doAttack();
+    }
+    // badGuy bad;
+
+    // badGuy* b1 = &n;
+    // badGuy* b2 = &p;
+    // // badGuy* b3 = &bad;
+
+    // b1->doAttack();
+    // b2->doAttack();
+    // // b3->doAttack();
+
+    //B b1(3);
+    // A* a = new D();
+    // a->eat();
 }
