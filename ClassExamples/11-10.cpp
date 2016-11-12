8484795b75dd5ae312ede70ee7f578c638c01838
travesto <tdc12c@acu.edu>
2016-11-12 09:39:17 -0600

    ClassExamples
    Design patterns

---
+++ ClassExamples/11-10.cpp
@@ -0,0 +1,8 @@
+/*
+    Design patterns
+    Strategy
+    -Encapsulating behaviour to select at run time
+    -Define a family of algorithms, encapsulate each one then make them interchangeable
+    -Lets algorithm be independent from client
+    
+*/
